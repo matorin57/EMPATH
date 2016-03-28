fb4de3105f004906cc720ab3618d55655dc0da87
Garrigan Stafford <gstafford@tamu.edu>
2016-03-27 22:55:34 -0500

    Added Images, Added black aesthetic

--- EMPATH/website/urls.py
+++ EMPATH/website/urls.py
@@ -1,9 +1,11 @@
-#contains url mathces
+﻿#contains url mathces
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.stories, name='index'),
-	url(r'^stories/$', views.stories, name='index'),
+	url(r'^stories/$', views.stories, name='stories'),
+    url(r'^opinions/$', views.opinions, name='opinions'),
+    url(r'^contact/$', views.contact, name='contact'),
 ]
--- EMPATH/website/views.py
+++ EMPATH/website/views.py
@@ -1,7 +1,15 @@
 ﻿from django.shortcuts import render
+from website.models import Video
 
 # Create your views here.
 def stories(request):
-	#Will Grab all video links here for main page
-	context = {}
-	render(request,'website\\stories.html',context)
+    #Will Grab all video links here for main page
+    videoList = Video.objects.all()
+    #print(videoList)
+    context = {"videoList":videoList}
+    return render(request,'website\\stories.html',context)
+
+def opinions(request):
+    return None
+def contact(request):
+    return None
